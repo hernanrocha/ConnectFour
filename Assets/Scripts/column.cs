35f715a0aad9f98c160c353cd08a2391840f05cb
herny1993 <hernangrocha@yahoo.com.ar>
2015-06-10 14:29:03 -0300

    Agregado tablero y evento de mouse sobre columnas

---
+++ Assets/Scripts/column.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class column : MonoBehaviour {
+
+	//public ConnectFour game;
+	public int id;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnMouseDown(){
+		// Colocar ficha en la columna
+		Debug.Log ("Colocar ficha en columna " + id);
+		//game.addTween (id);
+	}
+}
