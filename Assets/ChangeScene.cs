2874a634dbb5debbd2d213cc6cf16e4c3ff93869
herny1993 <hernangrocha@yahoo.com.ar>
2015-06-11 01:41:27 -0300

    Menu de inicio. Botones de transicion entre escenas

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Update is called once per frame
+	public void ChangeSceneTo (string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
